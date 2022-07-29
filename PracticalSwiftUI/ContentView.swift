3f7d3811547afd2cf9047f1a3ed49b8e0268e558
Datafox-JCP <datafox@outlook.com>
2022-07-29 12:33:03 -0500

    Initial Commit

---
+++ PracticalSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticalSwiftUI
+//
+//  Created by Juan Hernandez Pazos on 29/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticalSwiftUI/PracticalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalSwiftUIApp.swift
+//  PracticalSwiftUI
+//
+//  Created by Juan Hernandez Pazos on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PracticalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

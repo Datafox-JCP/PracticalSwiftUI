f0148b4994fde534689deea8dcdf2f12e736367b
Datafox-JCP <datafox@outlook.com>
2022-08-01 14:35:02 -0500

    - TestResultsView

--- PracticalSwiftUI/Views/TestView.swift
+++ PracticalSwiftUI/Views/TestView.swift
@@ -100,8 +100,8 @@ struct TestView: View {
             .navigationTitle("\(model.currentModule?.category ?? "") Test")
         }
         else {
-                // Test hasn't loaded yet
-            ProgressView()
+            // If current question is nil, we show the result view
+            TestResultView(numCorrect: numCorrect)
         }
     }
     

6a7fd490b1d462d50210f1b8fec17954dfe5b050
Datafox-JCP <datafox@outlook.com>
2022-08-02 08:23:22 -0500

    - Final bugs corrected

--- PracticalSwiftUI/Views/ContentView.swift
+++ PracticalSwiftUI/Views/ContentView.swift
@@ -10,15 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var model: ContentModel
     
-    var index: Int
-    var lesson: Lesson {
-        if model.currentModule != nil && index < model.currentModule!.content.lessons.count {
-            return model.currentModule!.content.lessons[index]
-        } else {
-            return Lesson(id: 0, title: "", video: "", duration: "", explanation: "")
-        }
-    }
-    
     var body: some View {
         ScrollView {
             LazyVStack {
